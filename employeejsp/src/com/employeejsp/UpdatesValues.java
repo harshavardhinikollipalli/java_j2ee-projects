cb28242e108df83dc65412ce76aec206dc671f03
vasam srikanth <srikanth.dhatriinfo@gmail.com>
2016-12-26 14:30:40 +0530

    used ajax in search update

--- employeejsp/src/com/employeejsp/UpdatesValues.java
+++ employeejsp/src/com/employeejsp/UpdatesValues.java
@@ -38,7 +38,7 @@ public class UpdatesValues extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String tempid=request.getParameter("search");
+		String tempid=request.getParameter("updateVariable");
 		
 		DAOOperations dbo=new DAOOperations();
 		
